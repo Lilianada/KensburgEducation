290ef7bfce60bb93e1c9290b22e1f5ce57f33554
Lilianada <lilibest30.lb@gmail.com>
2022-07-12 14:07:32 -0400

    Service section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <main className='bodyWrapper'>
         <Hero />
+        <Services />
       </main>
     </div>
   );
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Hero.css';
 import Logo from '../../assets/Kensburgeducation.png'
+import Image from '../../assets/wfh_9.svg'
 
 function Hero() {
   return (
-    <main className='heroWrapper'>
+    <div className='heroWrapper'>
       <img className='logo' src={Logo} alt='Logo' />
       <section className='flexBox'>
         <div className="flexOne">
+          <h4 className="colorText">Lorem Ipsum del luna</h4>
           <h1 className="heroText">
-            Learn to speak English fluently with fun, energetic and creative teachers.
+            Learn to speak English fluently with our online tutors.
           </h1>
-          
+          <button className='cto'>Start learning now</button>
         </div>
-        <div className="flexTwo"></div>
+        <div className="flexTwo">
+          <img src={Image} alt="Hero" />
+        </div>
       </section>
-    </main>
+    </div>
   )
 }
 
