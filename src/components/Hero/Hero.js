0acf6178ae23f7322b4d9597b03e5c43aaaf8b4a
Lilianada <lilibest30.lb@gmail.com>
2022-07-12 09:46:28 -0400

    Hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './Hero.css'
+import './Hero.css';
+import Logo from '../../assets/Kensburgeducation.png'
 
 function Hero() {
   return (
-    <main className=''>
-      <div className='logo'>MMMMMMDBUUUBV MAAaaaaaammmosooid</div>
+    <main className='heroWrapper'>
+      <img className='logo' src={Logo} alt='Logo' />
+      <section className='flexBox'>
+        <div className="flexOne">
+          <h1 className="heroText">
+            Learn to speak English fluently with fun, energetic and creative teachers.
+          </h1>
+          
+        </div>
+        <div className="flexTwo"></div>
+      </section>
     </main>
   )
 }
 
 export default Hero
+
